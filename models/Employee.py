4b1b281214649915c66610d1151a0126e2d91f2c
AksaJoshwa <aksajosh@gmail.com>
2025-08-12 16:37:34 +0530

    project created

---
+++ models/Employee.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self,name):
+        self.name=NameError
+
+    def display(self):
+        print(f"the name is:",self.name)
+
+
+#empty folders are not tracked and here the env is not tracked too
